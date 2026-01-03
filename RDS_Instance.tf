5a37ba3456474cbf55b9361b0bf7b26ce003019b
Kyle <229825307+Kiccal85@users.noreply.github.com>
2026-01-02 21:40:42 -0500

    Modify RDS instance settings and identifier
    Updated RDS instance configuration with new identifier and settings.

--- RDS_Instance.tf
+++ RDS_Instance.tf
@@ -1,15 +1,21 @@
 
-resource "aws_db_instance" "chewbacca_rds01" {
-  identifier             = "${local.name_prefix}-rds01"
+resource "aws_db_instance" "database_1" {
+  identifier             = "database-1"
   engine                 = var.db_engine
   instance_class         = var.db_instance_class
+  storage_type           = "gp2"
   allocated_storage      = 20
+  backup_retention_period = 7
   db_name                = var.db_name
   username               = var.db_username
   password               = var.db_password
+  multi_az               = true
+  delete_automated_backups = false
 
-  db_subnet_group_name   = aws_db_subnet_group.chewbacca_rds_subnet_group01.name
-  vpc_security_group_ids = [aws_security_group.chewbacca_rds_sg01.id]
+  # db_subnet_group_name   = "aws_db_subnet_group.chewbacca_rds_subnet_group01.name"
+  # vpc_security_group_ids = [aws_security_group.chewbacca_rds_sg01.id]
+
+  
 
   publicly_accessible    = false
   skip_final_snapshot    = true
@@ -17,6 +23,6 @@ resource "aws_db_instance" "chewbacca_rds01" {
   # TODO: student sets multi_az / backups / monitoring as stretch goals
 
   tags = {
-    Name = "${local.name_prefix}-rds01"
+    Name = "database"
   }
 }
